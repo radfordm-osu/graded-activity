0352d595552ea0378384d75ee10762f4c5cba013
radfordm-osu <77699896+radfordm-osu@users.noreply.github.com>
2021-03-11 09:08:20 -0800

    updated syntax

--- test_calculator.py
+++ test_calculator.py
@@ -1,10 +1,10 @@
-import calculator
 
+
+import calculator
 
 class TestCalculator:
     def test_add(self):
         assert 5 == calculator.addition(1, 4)
-
 
     def test_subtract(self):
         assert 2 == calculator.subtract(5, 3)
